4c9eac267360b44063da00997e5091570c58a496
yonshavit <48649051+yonshavit@users.noreply.github.com>
2019-12-18 13:31:22 +0200

    Added the script and it's dependency file

---
+++ html_changes_bash_script.sh
@@ -0,0 +1,14 @@
+sudo apt -y update
+sudo apt -y install python2.7
+sudo apt -y install python-pip
+pip2 install requests
+pip2 install boto3
+
+sudo apt -y install git-all
+git clone https://github.com/yonshavit/z_interview.git
+
+cd z_interview
+
+sudo cp -i ./webpage_watcher.py
+crontab ./crontab_file
+python2.7 webpage_watcher
